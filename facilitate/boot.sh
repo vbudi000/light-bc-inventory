828157245b5654ea3982ce252d9943575f26ea32
Kitty Catt <66872501+kitty-catt@users.noreply.github.com>
2020-06-27 12:05:09 +0200

    Update boot.sh

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -1,6 +1,6 @@
 export DOCKER_USERNAME='docker user'
 export GIT_USERNAME='git user'
-export BC_PROJECT="bc-9"
+export BC_PROJECT="bc-light"
 #export APPS_LB="apps-crc.testing"
 export APPS_LB="apps.vied.cp.fyre.ibm.com"
 
